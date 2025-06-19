a77cb8fa324577d8a577e5d13eda55366daeaa0c
cfmin09 <147251502+cfmin09@users.noreply.github.com>
2025-06-19 20:46:30 +0800

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,19 @@
 if [[ ${target_platform} == "linux-64" ]] || [[ ${target_platform} == "linux-aarch64" ]]; then
   sed -i 's/std::ios::streampos/std::streampos/g' src/View.cpp
 elif [[ ${target_platform} == "osx-64" ]]; then
+  sed -i '' "s/std::ios::streampos/std::streampos/g" src/View.cpp
   sed -i '' "s/std::auto_ptr/std::unique_ptr/g" db/include/db/Transaction.hpp
   sed -i '' "s/std::auto_ptr/std::unique_ptr/g" db/include/db/Connection.hpp
   sed -i '' "s/std::auto_ptr/std::unique_ptr/g" db/include/db/SQLStatement.hpp
+  sed -i '' "s/std::auto_ptr/std::unique_ptr/g" genfile/include/genfile/bgen/IndexQuery.hpp
+  sed -i '' "s/std::auto_ptr/std::unique_ptr/g" genfile/include/genfile/bgen/View.hpp
+  sed -i '' "s/std::auto_ptr/std::unique_ptr/g" appcontext/include/appcontext/OptionProcessor.hpp
+  sed -i '' "s/std::auto_ptr/std::unique_ptr/g" appcontext/include/appcontext/CmdLineUIContext.hpp
+  sed -i '' "s/std::auto_ptr/std::unique_ptr/g" appcontext/src/ApplicationContext.cpp
+  sed -i '' "s/std::auto_ptr/std::unique_ptr/g" apps/bgenix.cpp
+  sed -i '' "s/std::auto_ptr/std::unique_ptr/g" apps/cat-bgen.cpp
+  sed -i '' "s/std::auto_ptr/std::unique_ptr/g" apps/edit-bgen.cpp
+  sed -i '' "s/std::auto_ptr/std::unique_ptr/g" test/unit/catch.hpp
 fi
 
 ./waf configure build install \
